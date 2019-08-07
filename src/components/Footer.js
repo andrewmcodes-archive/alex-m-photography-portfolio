a8dbc43105272141c6c717d912f91703b14ee9f2
Andrew Mason <andrewmcodes@protonmail.com>
2019-08-07 18:08:00 -0400

    update social icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import Facebook from '../img/social/facebook-icon'
-import Twitter from '../img/social/twitter-icon'
-import Instagram from '../img/social/instagram-icon'
+import { FaRegEnvelope, FaInstagram } from "react-icons/fa";
 
 const Footer = class extends React.Component {
   render() {
@@ -9,14 +7,11 @@ const Footer = class extends React.Component {
       <footer className="p-4 sm:p-8 bg-indigo-600">
         <div className="w-full flex flex-col sm:flex-row-reverse justify-between">
           <div className="mx-auto sm:mx-0 w-1/2 flex justify-between self-center sm:justify-end mb-4 sm:mb-0">
-            <a title="facebook" href="https://facebook.com" className="px-4">
-              <Facebook />
+            <a title="email" href="mailto:alex.mcc95@gmail.com" className="px-4">
+              <FaRegEnvelope className="text-3xl text-white" />
             </a>
-            <a title="twitter" href="https://twitter.com" className="px-4">
-              <Twitter />
-            </a>
-            <a title="instagram" href="https://instagram.com" className="px-4">
-              <Instagram />
+            <a title="instagram" href="https://www.instagram.com/aemphoto.graphy/" className="px-4" taget="_blank">
+              <FaInstagram className="text-3xl text-white" />
             </a>
           </div>
           <div className="text-gray-100 w-full mx-auto text-center self-center sm:text-left text-sm sm:text-xs">
